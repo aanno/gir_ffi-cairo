fab8a0b62c1be16182ed1f1f8af3b1ef8a5ec4cb
Thomas Pasch <thomas.pasch@gmx.de>
2015-01-14 21:17:58 +0100

    some minimal cairo
    Signed-off-by: Thomas Pasch <thomas.pasch@gmx.de>

--- lib/gir_ffi-cairo/context.rb
+++ lib/gir_ffi-cairo/context.rb
@@ -2,6 +2,7 @@ module Cairo
   load_class :Context
 
   class Context
+    
     def self.create target
       ptr = Lib.cairo_create target
       self.wrap ptr
@@ -23,16 +24,84 @@ module Cairo
     def set_source_rgba red, green, blue, alpha
       Lib.cairo_set_source_rgba self, red, green, blue, alpha
     end
+
+    # context
+    # http://www.cairographics.org/manual/cairo-cairo-t.html
+    
+    def set_source_rgb red, green, blue
+      Lib.cairo_set_source_rgb self, red, green, blue
+    end
+    
+    def destroy
+      Lib.cairo_destroy self
+    end
+    
+    # paths
+    # http://www.cairographics.org/manual/cairo-Paths.html#cairo-move-to
+    
+    def line_to x, y
+      Lib.cairo_line_to self, x, y
+    end
+    
+    def move_to x, y
+      Lib.cairo_move_to self, x, y
+    end
+    
+    # text 
+    # http://www.cairographics.org/manual/cairo-text.html
+    
+    def select_font_face family, slant, weight
+      # Lib.cairo_select_font_face self, family, slant, weight
+    end
+    
+    def set_font_size size 
+      # Lib.cairo_set_font_size self, size
+    end
+    
+    def show_text utf8
+      Lib.cairo_show_text self, utf8
+    end
+
   end
 
   module Lib
     attach_function :cairo_create, [:pointer], :pointer
+    
     attach_function :cairo_get_target, [:pointer], :pointer
 
     attach_function :cairo_arc,
       [:pointer, :double, :double, :double, :double, :double], :void
+    
     attach_function :cairo_fill, [:pointer], :void
+    
     attach_function :cairo_set_source_rgba,
       [:pointer, :double, :double, :double, :double], :void
+    
+    # added by aanno
+    
+    # context
+    # http://www.cairographics.org/manual/cairo-cairo-t.html
+    
+    attach_function :cairo_set_source_rgb,
+      [:pointer, :double, :double, :double], :void
+    
+    attach_function :cairo_destroy, [:pointer], :void
+    
+    # paths
+    # http://www.cairographics.org/manual/cairo-Paths.html#cairo-move-to
+        
+    attach_function :cairo_line_to, [:pointer, :double, :double], :void
+    
+    attach_function :cairo_move_to, [:pointer, :double, :double], :void
+    
+    # text 
+    # http://www.cairographics.org/manual/cairo-text.html
+    
+    # attach_function :cairo_select_font_face [:pointer, :string, :double, :double], :void
+    
+    # attach_function :cairo_set_font_size [:pointer, :double], :void
+    
+    attach_function :cairo_show_text, [:pointer, :string], :void
+    
   end
 end
--- lib/gir_ffi-cairo/surface.rb
+++ lib/gir_ffi-cairo/surface.rb
@@ -3,9 +3,39 @@ module Cairo
 
   class Surface
     # TODO: Add overrides
+    
+    # context
+    # http://www.cairographics.org/manual/cairo-cairo-t.html
+    
+   def destroy
+      Lib.cairo_surface_destroy self
+    end
+    
+    alias :surface_destroy :destroy
+    
+    # PNG support
+    # http://cairographics.org/manual/cairo-PNG-Support.html
+    
+    def write_to_png filename
+      Lib.cairo_surface_write_to_png self, filename
+    end
+    
   end
 
   module Lib
     # TODO: Attach functions
+    #
+    # attach_function :cairo_image_surface_create, [Cairo::Format, :int, :int], :pointer
+    
+    # context
+    # http://www.cairographics.org/manual/cairo-cairo-t.html
+    
+    attach_function :cairo_surface_destroy, [:pointer], :void
+    
+    # PNG support
+    # http://cairographics.org/manual/cairo-PNG-Support.html
+    
+    attach_function :cairo_surface_write_to_png, [:pointer, :string], :void
+    
   end
 end
